9590c717a1fa779c7b2bcbb00393e88cf065b9d9
damienbod <damien_bod@hotmail.com>
2024-04-14 13:43:23 +0200

    Update UI and YARP

--- bff/ui/src/app/call-api/call-api.component.ts
+++ bff/ui/src/app/call-api/call-api.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-call-api',
@@ -8,4 +10,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './call-api.component.html',
   styleUrl: './call-api.component.scss',
 })
-export class CallApiComponent {}
+
+export class CallApiComponent {
+
+  private readonly httpClient = inject(HttpClient);
+  dataFromAzureProtectedApi$: Observable<string[]>;
+
+  getDirectApiData() {
+    this.dataFromAzureProtectedApi$ = this.httpClient.get<string[]>(
+      `${this.getCurrentHost()}/api/DirectApi`
+    );
+  }
+
+  private getCurrentHost() {
+    const host = window.location.host;
+    const url = `${window.location.protocol}//${host}`;
+
+    return url;
+  }
+}
